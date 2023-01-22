136ec958ce33dc7afb1dbfcd0657fdc636321dc7
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-01-22 19:56:56 +0530

    Adding a project

---
+++ simpleReactWebsite/README.md
@@ -0,0 +1,1 @@
+[Deployed Here](https://simple-website-green.vercel.app/)
---
+++ simpleReactWebsite/src/App.jsx
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Services from './components/Services'
+
+import './styles/App.scss'
+import './styles/Header.scss'
+import './styles/Footer.scss'
+import './styles/Home.scss'
+import './styles/Contact.scss'
+import './styles/mediaquery.scss'
+
+
+
+
+
+function App() {
+  return (
+    <Router>
+        <Header/>
+        <Routes>
+            <Route path="/" element= {<Home/>} />
+            <Route path="/contact" element= {<Contact/>} />
+            <Route path="/services" element= {<Services/>} />
+        </Routes>
+        <Footer/>
+    </Router>
+  )
+}
+
+export default App
